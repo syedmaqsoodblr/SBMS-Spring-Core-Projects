42f487902e08fe87f3e9454126d43b6c7fe1e56f
Syed Maqsood <syedmaqsoodblr@gmail.com>
2022-03-07 00:38:28 +0530

    Added ComponentScan annotation

--- 08-Spring-Boot-App/src/main/java/com/maq/Application.java
+++ 08-Spring-Boot-App/src/main/java/com/maq/Application.java
@@ -2,8 +2,10 @@ package com.maq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.maq", "in.maq" })
 public class Application {
 
 	public static void main(String[] args) {
